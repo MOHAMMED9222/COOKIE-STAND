d1830e8181bcf28965522b4031553044291ee1e6
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-01-31 14:43:28 -0800

    seattle

--- app.js
+++ app.js
@@ -25,36 +25,7 @@ let seattlestore = {
   avg: 6.3,
   SEAcookiessoldphr: [],
 
-render: function() {
-  this.SEAcookiessoldphr();
-  // method to render cookies sales as <ul>
-  // windows into the dom 
-  // us the id seattle sales
-let salescontainer = document.getElementById('seattlesales');
-
-let ulElement = document.createElement('ul');
-// give it content
-//append it to the dom
-salescontainer.appendChild(ulElement)
-
-
-
-// create a new element
-for (let i =0; i < hour.length; i++) {
-let lielement = document.createElement('li')
-// give it content '6am: 16 cookies'
-lielement.textContent = `${hours[i]}: ${SEAcookiessoldphr[i]}`
-// append it to the dom
-ulelement.appendChild(liElement);
-}
-let totallielement = document.createElement('li')
-// append it to the dom
-totallielement = `Total: ${this.dailytotal} cookies`;
-ulElement.append(totallielement)
-},
-
-
-getNumberOfRandomcustomersphr: function () {
+  getNumberOfRandomcustomersphr: function () {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
 
@@ -64,28 +35,60 @@ getNumberOfRandomcustomersphr: function () {
   
   // using average cookies ourchased
   // and the random number of customers
+  for (let i = 0; i < hour.length; i++) {
+    
+    let seattcustomers = this.getNumberOfRandomcustomersphr();
+    //console.log(seattcustomers)
+    
+    let cookiessoldthishour = Math.ceil(seattcustomers * this.avg); 
+   // console.log(cookiessoldthishour);
+    this.SEAcookiessoldphr.push(cookiessoldthishour);
+    
 
-
-  for (let i =0; i < hour.length; i++) {
-    let seattcustomersphr = this.getNumberOfRandomcustomersphr();
-
-  let seacookieavg = Math.ceil (seattcustomersphr * this.avg)
+  //let seacookieavg = Math.ceil (seattcustomers * this.avg)
   //console.log(seaAvgphr)
   
-  this.SEAcookiessoldphr.push(seaAvgphr)
+  //this.SEAcookiessoldphr.push(seaAvgphr)
 
   // calculating the sum of these hourly rates
   // x = x +1 
   //x += 1 
-  this.dailytotal += seacookieavg;
+  //this.dailytotal += seacookieavg;
 
   //console.log(this.SEAcookiessoldphr);
   //     console.log(calculatecookiessoldphr[i]);
   //    let numbersofcustomers = this.getNumberOfRandomcustomersphr()
- };
   }
+  console.log(this.SEAcookiessoldphr);
+ },
+ render: function() {
+  this.calculatecookiessoldphr();
+ 
+  let salescontainer = document.getElementById('seattlesales'); 
+ 
+
+  let ulElement = document.createElement('ul');
+  console.log(salescontainer);
+ 
+  salescontainer.appendChild(ulElement);
+
+  for (let i = 0; i < hour.length; i++) {
+
+ //create
+  let liElement = document.createElement('li');
+// content
+liElement.textContent = `${hour[i]}; ${this.SEAcookiessoldphr[i]} cookies`;
+
+// append
+ulElement.appendChild(liElement);
+ }
+ }
 }
-console.log(seattlestore.getNumberOfRandomcustomersphr());
+
+seattlestore.calculatecookiessoldphr();
+seattlestore.render();
+//console.log(seattlestore.calculatecookiessoldphr());
+//console.log(seattlestore.getNumberOfRandomcustomersphr());
 
 // console.log(seattlestore.calculatecookiessoldphr());
 
@@ -169,6 +172,34 @@ console.log(seattlestore.getNumberOfRandomcustomersphr());
 
 
 
+
+// render: function() {
+//   this.SEAcookiessoldphr();
+//   // method to render cookies sales as <ul>
+//   // windows into the dom 
+//   // us the id seattle sales
+// let salescontainer = document.getElementById('seattlesales');
+
+// let ulElement = document.createElement('ul');
+// // give it content
+// //append it to the dom
+// salescontainer.appendChild(ulElement)
+
+
+
+// // create a new element
+// for (let i =0; i < hour.length; i++) {
+// let lielement = document.createElement('li')
+// // give it content '6am: 16 cookies'
+// lielement.textContent = `${hours[i]}: ${SEAcookiessoldphr[i]}`
+// // append it to the dom
+// ulelement.appendChild(liElement);
+// }
+// let totallielement = document.createElement('li')
+// // append it to the dom
+// totallielement = `Total: ${this.dailytotal} cookies`;
+// ulElement.append(totallielement)
+// },
 
 
 
